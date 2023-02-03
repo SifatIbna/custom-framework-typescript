4cf825bbd5908c967555b23b020525615a0dd4e3
SifatIbna <sifatibna.amin9@gmail.com>
2023-02-03 11:25:47 +0600

    added functionality in User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const user = new User({ name: "myName", age: 20 });
+
+user.on("change", () => {
+  console.log("change callback");
+});
+user.on("change", () => {
+  console.log("change callback1");
+});
+
+user.trigger("change");
---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+  get(propName: keyof UserProps): string | number | undefined {
+    return this.data[propName];
+  }
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length == 0) {
+      return;
+    }
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+}
